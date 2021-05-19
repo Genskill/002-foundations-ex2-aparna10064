0a4e554d94e4ee65e1bab00309ee85b1fd21e75a
aparna10064 <72874041+aparna10064@users.noreply.github.com>
2021-05-19 21:17:31 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -6,9 +6,9 @@ int p = get_int("Enter type of pattern (1 or 2)");
 int row =get_int("Number of rows");
 if(p==1)
 {
-	for(int i=row;i>0;i--)
+	for(int i=row ;i>0 ;i--)
 	{
-		for(int j = i;j > 0; j--);
+		for(int j=i ;j > 0; j--);
 		{
 		printf("#");
 		}
@@ -18,7 +18,7 @@ if(p==1)
 else if(p==2)
 {
 
-	for(int i= 0 ; i<rows ; i++)
+	for(int i= 0 ; i<row ; i++)
 	{
 		for(int k=row -i-1 ; k>0 ; k--);
 		{
