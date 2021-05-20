9612e94191568d5ec4067709870d1019dd59f623
aparna10064 <72874041+aparna10064@users.noreply.github.com>
2021-05-20 19:08:54 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -8,33 +8,33 @@
  int rows = get_int("Number of rows ");
  if(type == 1)
  {
-   for(i = 1; i <= rows; i++)
-   {
-	   for(j = rows; j >= i; j--)
+   for(i = rows; i > 0; i--){
+	   for(j = 0; j < i; j++)
 	   {
 		   prinf("#");
 	   }
 	   printf("\n");
    }
- }
-else if(type == 2)
-{
-	for(i = rows; i >= 1; i--)
+ 
+ } else if(type == 2){
+	 
+	for(i = rows; i > 0; i--)
 	{
-		for(j = 1; j < rows; j++)
+		for(j = 0; j < rows; j++)
 		{
-			if(j <= i-1)
-			
-			printf(" ");
-		  
-			else
+			if(j >= i-1){
 			
 			printf("#");
+		  
+			} else
+			
+			printf(" ");
+			
 			}
 		printf("\n");
-		}
+		
 	}
-  else
-    prinf("error");
+ } else
+    printf("error");
 }
  
